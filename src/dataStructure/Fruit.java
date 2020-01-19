e622b71d48d99a5df0ded5821be69cbf9c26b4dd
lidorT <user@DESKTOP-MQM5DDS>
2020-01-19 23:48:43 +0200

    Fixing conflict Package dataStructer

--- src/dataStructure/Fruit.java
+++ src/dataStructure/Fruit.java
@@ -1,111 +1,205 @@
 package dataStructure;
 
-
-import java.awt.image.BufferedImage;
-
 import org.json.JSONObject;
 import utils.Point3D;
-import utils.*;
-import gameClient.*;
+/**
+ * ~~Fruit Class~~
+ ** This class represents the Fruits of the game.
+ *	the idea is to collect those fruits in order to get a better score (grades).
+ *	each fruit has two type of values that represents from where you can collect it.
+ * 	
+ * 
+ * @author Zohar & Lidor
+
+ * Class Fields :
+ * 
+ * 
+ * value- the points you recive for collecting this fruit.
+ * tpye - each fruit has two type of values(1,-1) that represents from where you can collect it..
+ * pos  - a 3DPoint to hold the fruit location.
+ * edge -  holde the edge that the fruit is placeed on.
+ * ClosestNode - holds the the node that is closest to the fruit.
+ * src - holds the fruit source node key.
+ * dest -  holds the destination node's key of the fruit.
+ * rot - "rot" means robot on the move - a boolean field to mark of a robot is
+ * 		 already sent to this specific fruit or not.
+ * dist - holds a distance as a double to calculate distance later on.
+ */
 public class Fruit {
 
 	private int value;
 	private int type;
 	private Point3D pos;
 	private edge_data edge;
-	BufferedImage myImage;
 	private node_data ClosestNode;
 	private int src;
 	private int dest;
 	public boolean Rot = false;  // Robot on the way
-	
-	public int getSrc() {
-		return src;
-	}
-
-	public void setSrc(int src) {
-		this.src = src;
-	}
-
-	public int getDest() {
-		return dest;
-	}
-
-	public void setDest(int dest) {
-		this.dest = dest;
-	}
-
 	public double dist;
 
+	/**
+	 * Default constructor - sets all values to 0 and null , type to 1.
+	 */
 	public Fruit(){
 		this.value = 0;
 		this.type = 1;
 		this.pos=null;
 		this.edge=null;
 	}
-	
-	public double getDist() {
-		return dist;
-	}
 
-	public void setDist(double dist) {
-		this.dist = dist;
-	}
-
+	/**
+	 *  Specific constructor.
+	 * @param val - value of the fruit
+	 * @param type - type of the fruit (1 or -1 )
+	 * @param pos - 3D point to hold the fruit location.
+	 * @param edge - the edge that the fruit is placed on.
+	 */
 	public Fruit(int val,int type, Point3D pos, edge_data edge){
 		this.value = val;
 		this.type = type;
 		this.pos = pos;
 		this.edge = edge;
 	}
+	/**
+	 * 
+	 * @return the key of the source node of the fruit.
+	 */
+	public int getSrc() {
+		return src;
+	}
+	/**
+	 * This methods sets the source node id of the fruit.
+	 * @param - an integer to represent the key of the source node of the fruit.
+	 */
+	public void setSrc(int src) {
+		this.src = src;
+	}
 	
+	/**
+	 * 
+	 * @return the key of the destination node of the fruit.
+	 */
+	public int getDest() {
+		return dest;
+	}
+	
+	/**
+	 * This methods sets the destination node id of the fruit.
+	 * @param - an integer to represent the key of the destination node of the fruit.
+	 */
+	public void setDest(int dest) {
+		this.dest = dest;
+	}
+	
+	/**
+	 * 
+	 * @return -  a double to represent distance for future calculations.
+	 */
+	
+	public double getDist() {
+		return dist;
+	}
+	
+	/**
+	 * This method idea is to hold a double that represent distance.
+	 * 
+	 * @param dist - a double to set the distance.
+	 */
+	
+	public void setDist(double dist) {
+		this.dist = dist;
+	}
+	
+	/**
+	 * 
+	 * @return - the value of the fruit ( the points ones get from picking this fruit).
+	 */
 	public int getValue(){
 		return value;
 	}
+	
+	/**
+	 *  Set the value of the fruit.
+	 * @param value - an integer to represent the fruit value's.
+	 */
 	
 	public void setValue(int value){
 		this.value = value;
 	}
 	
+	/**
+	 *  Sets the type of the fruit, -1 of you can pick the fruit only from lower vertex
+	 *  to a higher one ( from 8 to 9 ) and 1 if you can only pick the fruit from a higher
+	 *  vertex fo a lower one.
+	 * @param type -         1 / -1  as written  above.
+	 */
 	public void setType(int type){
 		this.type = type;
 	}
 	
+	/**
+	 * 
+	 * @return the tpye of the fruit.
+	 */
 	public int getType(){
 		return type;
 	}
-
+	
+	/**
+	 * 
+	 * @return - returns a 3D point that represents the fruit location.
+	 */
 	public Point3D getPos(){
 		return pos;
 	}
 	
+	/**
+	 * A 3D Point that represents the fruit location.
+	 * @param pos -  3D Points to set the location.
+	 */
 	public void setPos(Point3D pos){
 		this.pos = pos;
 	}
 	
+	/**
+	 * 
+	 * @return - returns the edge that the fruit is placed on.
+	 */
 	public edge_data getEdge(){
 		return edge;
 	}
 	
+	/**
+	 *  Sets an edge_data edge that the fruit is placed on her.
+	 * @param edge -  the edge that wanted to be set.
+	 */
 	public void setEdge(edge_data edge) {
 		this.edge = edge;
 	}
 
-	
-	
-
+	/**
+	 * 
+	 * @return - returns the closest node to the fruit.
+	 */
 	public node_data getClosestNode() {
 		return ClosestNode;
 	}
-
+	
+	/**
+	 * This methods sets the closest node to the fruit.
+	 * @param closestNode - a node_data that will be set as the closest node to the fruit.
+	 */
 	public void setClosestNode(node_data closestNode) {
 		ClosestNode = closestNode;
 	}
-	
-	
-	
+
+	/**
+	 * This method gets a string as input and create a new fruit from the values inside the string
+	 * using a JSON.
+	 * @param g - a string that contains the values of the fruit.
+	 */
 	public void initFruit(String g){
-		
+
 		if(!g.isEmpty()){
 			try{
 				JSONObject obj = new JSONObject(g);
@@ -124,35 +218,7 @@ public class Fruit {
 			catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-			
-			
-			
-			
-			
+
 		}
 	}
-	
-	
-	
-
-
-
-	/**
-	 * get the image of the object
-	 * @return the myImage
-	 */
-	public BufferedImage getMyImage() {
-		return myImage;
-	}
-
-	/**
-	 * @param myImage - to set the image of the object
-	 */
-	public void setMyImage(BufferedImage myImage) {
-		this.myImage = myImage;
-	}
-
-
-	
 }
--- src/dataStructure/Robot.java
+++ src/dataStructure/Robot.java
@@ -1,27 +1,44 @@
 package dataStructure;
 
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.json.JSONObject;
-
 import utils.Point3D;
+/**
+ * ~~Robot Class~~
+ ** This class represents the Robots of the game.
+ *	One can control the Robots Manually and collect the fruits of the game
+ *	One can also run this game Automatically and watch how the game runs the robots
+ * 	and collect the fruits.
+ * 
+ * @author Zohar & Lidor
 
+ * Class Fields :
+ * 
+ * 
+ * id - an integer to represent the robot id.
+ * value - each robot has a value field he gets from the server(mostlyNull)
+ * pos - a 3DPoint to hold the robot location.
+ * speed - holds the movement speed of the robot.
+ * src -   holds the robot source node.
+ * SrcId - holds the key of the source node.
+ * dest -  holds the destination node of the robot.
+ * edge -  holde the edge that the robot is on.
+ * moving - determine of the robot is moving now or he is not.
+ */
 public class Robot {
-	
+
 	private int id;
 	private int value;
 	private Point3D pos;
 	private double speed;
 	private node_data src;
 	private int SrcId;
- 	private node_data dest;
+	private node_data dest;
 	private edge_data edge;
-	private BufferedImage image;
 	public boolean moving = false;
-	public List<node_data> moveList = new ArrayList<node_data>();
 
+	/**
+	 * Default constructor, sets all values to 0 and nulls.
+	 */
 	public Robot(){
 		this.id = 0;
 		this.pos = null;
@@ -30,77 +47,111 @@ public class Robot {
 		this.src = null;
 		this.dest=null;
 	}
-	
+	/**
+	 * Specific constructor, sets all values to the input ones.
+	 * @param id - an int to represent the robot id.
+	 * @param pos - a 3DPoint to set the robot location.
+	 * @param speed - sets the movement speed of the robot.
+	 * @param node - sets the robot src node to the input.
+	 */
 	public Robot(int id,Point3D pos,double speed,node_data node){
 		this.id = id;
 		this.pos = pos;
 		this.speed = speed;
 		this.src = node;
 	}
-
+	/**
+	 * 
+	 * @return the robot id.
+	 */
 	public int getId(){
 		return id;
 	}
-	
+	/**
+	 * 
+	 * @param id - sets this robot id to the input integer.
+	 */
 	public void setId(int id){
 		this.id = id;
 	}
-	
+	/**
+	 * 
+	 * @return - returns the location of the robot as a 3D Point.
+	 */
 	public Point3D getPos() {
 		return pos;
 	}
-	
+	/**
+	 * 
+	 * @param pos - sets the robot location to the input 3D location.
+	 */
 	public void setPos(Point3D pos) {
 		this.pos = pos;
 	}
-	
+	/**
+	 * 
+	 * @return the speed of the robot movement.
+	 */
 	public double getSpeed() {
 		return speed;
 	}
-	
+	/**
+	 * 
+	 * @param speed - sets the speed of the robot's movement.
+	 */
 	public void setSpeed(double speed) {
 		this.speed = speed;
 	}
-	
+	/**
+	 * 
+	 * @return the source node of the robot.
+	 */
 	public node_data getSrc() {
 		return src;
 	}
-	
+	/**
+	 * 
+	 * @param node - sets the source node of the robot.
+	 */
 	public void setSrc(node_data node) {
 		this.src = node;
 	}
-	
-	
+
+	/**
+	 * 
+	 * @return - returns the destination node of the robot.
+	 */
 	public node_data getDest() {
 		return dest;
 	}
-	
+	/**
+	 * 
+	 * @param node -  sets the destination node of the robot.
+	 */
 	public void setDest(node_data node) {
 		this.dest = node;
 	}
-	
+	/**
+	 * 
+	 * @return - returns the edge that the robot placed on.
+	 */
 	public edge_data getEdge() {
 		return edge;
 	}
-	
+	/**
+	 * 
+	 * @param edge - sets the node edge field to the input one.
+	 */
 	public void setEdge(edge_data edge) {
 		this.edge = edge;
 	}
-	
-	/**
-	 * @return the image
-	 */
-	public BufferedImage getImage() {
-		return image;
-	}
+
 
 	/**
-	 * @param image the image to set
+	 * This method gets a String from a json file which will be usually
+	 *  sent from the game server and builds a robot with the values it gets from the server.
+	 * @param json - a string that holds the robots values. 
 	 */
-	public void setImage(BufferedImage image) {
-		this.image = image;
-	}
-
 	public void RobotFromJSON(String json)
 	{
 		if(!json.isEmpty())
@@ -109,7 +160,7 @@ public class Robot {
 			{
 				JSONObject obj = new JSONObject(json);
 				JSONObject CurrBot = (JSONObject) obj.get("Robot");
-				
+
 				String pos = CurrBot.getString("pos");
 				String[] arr = pos.split(",");
 				double x = Double.parseDouble(arr[0]);
@@ -119,25 +170,39 @@ public class Robot {
 				int id = CurrBot.getInt("id");
 				this.id = id;
 				int value = CurrBot.getInt("value");
-				this.value = value;
+				this.setValue(value);
 				int speed = CurrBot.getInt("speed");
 				this.speed = speed;
 				int src = CurrBot.getInt("src");
 				this.setSrcId(src);
-				
+
 			}
 			catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
-
+	/**
+	 * 
+	 * @return - returns the robot source node key.
+	 */
 	public int getSrcId() {
 		return SrcId;
 	}
-
+	/**
+	 * 
+	 * @param srcId - sets the robots source Id to be as inserted.
+	 */
 	public void setSrcId(int srcId) {
 		SrcId = srcId;
 	}
 	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+
 }
